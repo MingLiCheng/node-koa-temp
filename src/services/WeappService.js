6fec399de63461fc2b9b586f57b9e5791dbdd71c
MingLiCheng <cml321@hotmail.com>
2020-09-07 17:10:24 +0800

    修复 获取ticket 错误的问题

--- src/services/WeappService.js
+++ src/services/WeappService.js
@@ -1,17 +1,22 @@
 const { weappRedis } = require('../../plugins/index')
 const request = require('request');
 const { weapp } = require('../../config/index')
+console.log('weapp', weapp);
+
 const sign = require('../../utils/weapp/sign');
 async function getAccessToken() {
-  const access_token = weappRedis.get('access_token')
+  const access_token = await weappRedis.get('access_token')
+  console.log('access_token', access_token);
+
   if (!!access_token) {
     return access_token
   } else {
     return new Promise((resolve, reject) => {
-      request(`https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${weapp.appid}&secret=${weapp.secret}`,
+      request(`https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${weapp.appId}&secret=${weapp.secret}`,
         async (error, response, body) => {
           if (!error && response.statusCode == 200) {
             let accessTokenInfo = JSON.parse(body)
+            console.log('accessTokenInfo', accessTokenInfo);
             weappRedis.set('access_token', accessTokenInfo.access_token, 'EX', accessTokenInfo.expires_in)
             resolve(accessTokenInfo.access_token)
           } else {
